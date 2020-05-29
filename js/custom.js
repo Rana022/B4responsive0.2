4ee99718e1d90ff8b9e9b1b309acc2f2d5f49e7b
Ghosh022 <ghosh1706022@gmail.com>
2020-05-29 22:50:24 +0600

    added offcanvas nav

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,24 @@ var scroll = new SmoothScroll('a[href*="#"]', {
     speed: 3000,
     speedAsDuration: true
 });
+
+// offcanvas
+$(document).ready(function() {
+    var fixHeight = function() {
+      $('.navbar-nav').css(
+        'max-height',
+        document.documentElement.clientHeight - 150
+      );
+    };
+    fixHeight();
+    $(window).resize(function() {
+      fixHeight();
+    });
+    $('.navbar .navbar-toggler').on('click', function() {
+      fixHeight();
+    });
+    $('.navbar-toggler, .overlay').on('click', function() {
+      $('.mobileMenu, .overlay').toggleClass('open');
+    });
+  });
+  
